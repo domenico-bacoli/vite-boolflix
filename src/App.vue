5a0d60e207daa2800507b2ff7ac9c26e5617c6b0
domenicobacoli <domenico.bacoli89@gmail.com>
2023-03-30 17:40:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from './store';
+import AppHeader from './components/AppHeader.vue';
+
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  components: {
+    AppHeader,
+  }
+}
+
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    data() {
+        return {
+            store,
+        };
+    },
+
+    components: {
+
+    }
+}
+</script>
+
+<template>
+    <div class="movies-search">
+        <input v-model="store.userValueInput" type="text" placeholder="Inserisci un Titolo">
+        <button>CERCA</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.movies-search {
+    display: flex;
+    justify-content: center;
+    flex-flow: row nowrap;
+    gap: 50px;
+    margin: 40px 0;
+
+    input,
+    button {
+        padding: 10px 20px;
+        border-radius: 10px;
+        border: none;
+    }
+}
+</style>
